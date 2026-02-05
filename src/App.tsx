f2b0310238d50ca41743b9f0a5870ba77adab31e
XaAnza <jaxote@gmail.com>
2026-02-05 13:39:47 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,37 @@
+import { useState } from "react";
+import { translations } from "./translations";
+import type { Language } from "./types";
+
 function App() {
+  const [lang, setLang] = useState<Language | null>(null);
+
+  if (!lang) {
+    return (
+      <div
+        style={{
+          minHeight: "100vh",
+          background: "black",
+          color: "white",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "2rem",
+          fontFamily: "Arial, sans-serif",
+        }}
+      >
+        <h1 style={{ color: "#e6007e" }}>
+          {translations.es.select_language}
+        </h1>
+
+        <div style={{ display: "flex", gap: "1rem" }}>
+          <button onClick={() => setLang("es")}>CASTELLANO</button>
+          <button onClick={() => setLang("eu")}>EUSKERA</button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div
       style={{
@@ -11,7 +44,7 @@ function App() {
         fontFamily: "Arial, sans-serif",
       }}
     >
-      <h1>Hala Bedi – funciona 🎉</h1>
+      <h1>{translations[lang].home}</h1>
     </div>
   );
 }
