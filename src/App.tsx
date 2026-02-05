03c64ac26b22aa860e05cbb6e266b5cb20d34648
XaAnza <jaxote@gmail.com>
2026-02-05 13:28:54 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "black",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Arial, sans-serif",
+      }}
+    >
+      <h1>Hala Bedi – funciona 🎉</h1>
+    </div>
+  );
+}
+
+export default App;
